7846eb29d274adef6cad9df50b28b0c70aefb1c6
Hannah-the-dev <hannah.the.dev@gmail.com>
2021-05-30 18:13:01 +0900

    a missed part updated

--- src/ticketStats/ReadData.java
+++ src/ticketStats/ReadData.java
@@ -4,7 +4,7 @@ public class ReadData {
 	public static final String adding = "INSERT INTO `ticketing` "
 			+ "(`date`, `ticket_type`, `age`, `quantity`, `amount`, `disocunt`) VALUES "; 
 	public static final String className = "com.mysql.cj.jdbc.Driver";
-	public static final String[] connectDB = {"jdbc:mysql://127.0.0.1:3306/testdb", 
-									"root",
-									"99164123"};
+	public static final String[] connectDB = {"jdbc:mysql://192.168.23.17:3306/kopoctc", 
+											"root",
+											"kopoctc"};
 }
